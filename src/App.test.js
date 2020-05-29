9a4c4718abd8f0b3224ce29cb3f72afb5f63c17f
kimj0588 <47002247+kimj0588@users.noreply.github.com>
2020-05-29 11:42:20 +0530

    Vulnerability

--- src/App.test.js
+++ src/App.test.js
@@ -25,7 +25,7 @@ afterEach(() => {
 
 
 it("post to updating value database test",()=>{
-  axios.post('https://xxxxxxx/forecast-values.json',{
+  axios.post('https://xxxxxxxjsonfilexxxxxxx',{
     forecast:1.0,
     actualValue:1.0
   })
